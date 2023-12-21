a67f7b6bfd521a14820534e774fe302164bc1f20
Radim Hruška <radim-hruska@outlook.com>
2023-12-21 17:45:21 +0100

    index is done

--- index.php
+++ index.php
@@ -13,13 +13,80 @@
 
 
     <img src="pic/taborak.png" alt="">
-    <div id="container">   
+      
 <div id="obsah"> 
-
-
-ewreree
+<div class="container"> 
+    <img src="pic/pic1.png" width="500px" style="margin:20px; margin-top: 50px;">
+    <div>
+        <h1>Nadpis</h1>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam id dolor. Maecenas lorem. In convallis.
+             Morbi scelerisque luctus velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Fusce wisi. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Cum sociis natoque penatibus et magnis dis parturient montes,
+               nascetur ridiculus mus. Duis bibendum</p>
+    </div>
+    
 </div>
+<hr>
+<div class="container"> 
+    
+    <div>
+        <h1>Nadpis</h1>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+             Aliquam id dolor. Maecenas lorem. In convallis. Morbi scelerisque luctus velit.
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+               Fusce wisi. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Cum sociis natoque penatibus et 
+               magnis dis parturient montes, nascetur ridiculus mus. Duis bibendum</p>
+    </div>
+    <img src="pic/pic2.png" width="500px" style="margin:20px; margin-top: 50px;">
+</div>
+<hr>
+<div class="container"> 
+    <img src="pic/pic3.png" width="500px" style="margin:20px; margin-top: 50px;">
+    <div>
+        <h1>Nadpis</h1>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam id dolor. Maecenas lorem. In convallis.
+             Morbi scelerisque luctus velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Fusce wisi. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Cum sociis natoque penatibus et magnis dis parturient montes,
+               nascetur ridiculus mus. Duis bibendum</p>
+    </div>
+    
+</div>
+    </div>
+    <div id="obsahWhite"> 
+        <h1>Naše termíny</h1>
+    <div class="containerWhite"> 
+    <div class="termin">
+        <img src="pic/nahl1.png" alt="" width="170px" style="margin: 20px; border-radius: 20px;">
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        
+    </div>
+    <div class="termin">
+    <img src="pic/nahl2.png" alt="" width="170px" style="margin: 20px; border-radius: 20px;">
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
 
+    </div>
+    <div class="termin">
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        
+    </div>
+    <div class="termin">
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        <h1>Nadpis</h1>
+        
+    </div>
     </div>
     <?php include("footer.php"); ?>
 </body>
--- nav.php
+++ nav.php
@@ -1,16 +1,16 @@
 <link href='https://fonts.googleapis.com/css?family=Roboto Condensed' rel='stylesheet'>
 <div id="container2">
-<a href="index.php" id="picLogo"> <img src="pic/logoMain.png" alt="logo" height="200em"></a>
+<a href="index.php" id="picLogo" > <img src="pic/logoMain.png" alt="logo" height="200em"></a>
 <div id="baner">DĚTSKÉ TÁBORY TORK</div>
 
     <nav>  
         <ul>
         <li><a href="index.php">Domů</a></li>
-        <li><a href="kontakt.php">Tábory</a></li>    
-        <li><a href="onas.php">Náš Team</a></li>
-        <li><a href="poskytujeme.php">Histrie tábora</a></li>
-        <li><a href="objednavame.php">Galerie</a></li>
-        <li><a href="ruzne.php">Kontakt</a></li>
+        <li><a href="#">Tábory</a></li>    
+        <li><a href="#">Náš Team</a></li>
+        <li><a href="#">Histrie tábora</a></li>
+        <li><a href="#">Galerie</a></li>
+        <li><a href="#">Kontakt</a></li>
     
     
         </ul>  
