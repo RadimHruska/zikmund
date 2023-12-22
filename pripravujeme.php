18b926952d17fb6ba76920343258f926638c4a81
RadimHruska <hruska@bbnsro.info>
2023-12-22 16:27:39 +0100

    pripravujeme done

--- pripravujeme.php
+++ pripravujeme.php
@@ -15,71 +15,79 @@
     
       
 <div id="obsahTeam"> 
-<div class="container"> 
+    <div class="containerCenter"> 
+        <div>
+            <h1 class="velkyText">Letos pro vás připravujeme</h1>
+        </div>
+    
+    </div>
+</div>
+<div class="obsahWhite">
+  <h1 style="font-size: 4em; text-align: center;"> <b> Nadpis odstavce </b></h1>
+    <div class="containerWhitePl">  
+        
+        <h2>30.12.2032</h2>
+        <h2>30.12.2032</h2>
+        <h2>30.12.2032</h2>
+    </div>
+    <div class="containerBlck"> 
+    
     <div>
-        <h1 class="velkyText">Letos pro vás připravujeme</h1>
+        <h1>Nadpis</h1>
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam id dolor. Maecenas lorem. In convallis.
              Morbi scelerisque luctus velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
               Fusce wisi. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Cum sociis natoque penatibus et magnis dis parturient montes,
                nascetur ridiculus mus. Duis bibendum</p>
-        
+        <p class="zvP">
+               <a href="#" >Ke stažení</a>
+               </p>
     </div>
-    
+    <img src="pic/pic1.png" width="400px" style="margin:20px; margin-top: 50px;" class="ZvPc">
 </div>
 <hr>
-<div class="container"> 
-    <img src="pic/team.png" width="300px" style="margin:20px; margin-top: 50px;" class="ZvPc">
+<h1 style="font-size: 4em; text-align: center;"> <b> Nadpis odstavce </b></h1>
+<div class="containerWhitePl"> 
+        <h2>30.12.2032</h2>
+        <h2>30.12.2032</h2>
+        <h2>30.12.2032</h2>
+    </div>
+    <div class="containerBlck"> 
+    
     <div>
-        <h1 class="velkyText">Kapitán</h1>
+        <h1>Nadpis</h1>
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam id dolor. Maecenas lorem. In convallis.
              Morbi scelerisque luctus velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
               Fusce wisi. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Cum sociis natoque penatibus et magnis dis parturient montes,
                nascetur ridiculus mus. Duis bibendum</p>
-        
+        <p class="zvP">
+               <a href="#" >Ke stažení</a>
+               </p>
     </div>
-    
+    <img src="pic/pic1.png" width="400px" style="margin:20px; margin-top: 50px;" class="ZvPc">
 </div>
 <hr>
-<div class="container"> 
-    <img src="pic/team.png" width="300px" style="margin:20px; margin-top: 50px;" class="ZvPc">
-    <div>
-        <h1 class="velkyText">Kapitán</h1>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam id dolor. Maecenas lorem. In convallis.
-             Morbi scelerisque luctus velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-              Fusce wisi. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Cum sociis natoque penatibus et magnis dis parturient montes,
-               nascetur ridiculus mus. Duis bibendum</p>
-        
+<h1 style="font-size: 4em; text-align: center;"> <b> Nadpis odstavce </b></h1>
+<div class="containerWhitePl"> 
+        <h2>30.12.2032</h2>
+        <h2>30.12.2032</h2>
+        <h2>30.12.2032</h2>
     </div>
+    <div class="containerBlck"> 
     
-</div>
-<hr>
-<div class="container"> 
-    <img src="pic/team.png" width="300px" style="margin:20px; margin-top: 50px;" class="ZvPc">
     <div>
-        <h1 class="velkyText">Kapitán</h1>
+        <h1>Nadpis</h1>
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam id dolor. Maecenas lorem. In convallis.
              Morbi scelerisque luctus velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
               Fusce wisi. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Cum sociis natoque penatibus et magnis dis parturient montes,
                nascetur ridiculus mus. Duis bibendum</p>
-        
-    </div>
-    
-</div>
-<hr>
-<div class="container"> 
-    <img src="pic/team.png" width="300px" style="margin:20px; margin-top: 50px;" class="ZvPc">
-    <div>
-        <h1 class="velkyText">Kapitán</h1>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam id dolor. Maecenas lorem. In convallis.
-             Morbi scelerisque luctus velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-              Fusce wisi. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Cum sociis natoque penatibus et magnis dis parturient montes,
-               nascetur ridiculus mus. Duis bibendum</p>
-        
-    </div>
+        <p class="zvP">
+               <a href="#" >Ke stažení</a>
+               </p>
+    </div><img src="pic/pic1.png" width="400px" style="margin:20px; margin-top: 50px;" class="ZvPc">
     
 </div>
 
-    </div>
+</div>
     <?php include("footer.php"); ?>
 </body>
 </html>
